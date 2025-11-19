93aee882b05e66aa73360830f2a85f0b51fd7d7b
akheruzzaman <akher.ice07@gmail.com>
2025-11-19 13:34:58 +0600

    Sentiment Analysis

--- src/TextCompletion/Program.cs
+++ src/TextCompletion/Program.cs
@@ -46,26 +46,45 @@ var client = new OpenAIClient(credintial, options).GetChatClient("openai/gpt-4.1
 
 #endregion
 
-
 #region Classification
 
-var classificationPrompt = """
-    Please classify the following sentences into categories: 
-    - 'complaint' 
-    - 'suggestion' 
-    - 'praise' 
-    - 'other'.
+//var classificationPrompt = """
+//    Please classify the following sentences into categories: 
+//    - 'complaint' 
+//    - 'suggestion' 
+//    - 'praise' 
+//    - 'other'.
 
-    1) "I love the new layout!"
-    2) "You should add a night mode."
-    3) "When I try to log in, it keeps failing."
-    4) "This app is decent."
-    """;
+//    1) "I love the new layout!"
+//    2) "You should add a night mode."
+//    3) "When I try to log in, it keeps failing."
+//    4) "This app is decent."
+//    """;
 
-Console.WriteLine($"user >> {classificationPrompt}");
+//Console.WriteLine($"user >> {classificationPrompt}");
 
-ChatResponse classificationResponse = await client.GetResponseAsync(classificationPrompt);
+//ChatResponse classificationResponse = await client.GetResponseAsync(classificationPrompt);
 
-Console.WriteLine($"assistant >> {classificationResponse}");
+//Console.WriteLine($"assistant >> {classificationResponse}");
+
+#endregion
+
+#region Sentiment Analysis
+
+var analysisPrompt = """
+        You will analyze the sentiment of the following product reviews. 
+        Each line is its own review. Output the sentiment of each review in a bulleted list and then provide a generate sentiment of all reviews.
+
+        I bought this product and it's amazing. I love it!
+        This product is terrible. I hate it.
+        I'm not sure about this product. It's okay.
+        I found this product based on the other reviews. It worked for a bit, and then it didn't.
+        """;
+
+Console.WriteLine($"user >>> {analysisPrompt}");
+
+ChatResponse responseAnalysis = await client.GetResponseAsync(analysisPrompt);
+
+Console.WriteLine($"assistant >>> \n{responseAnalysis}");
 
 #endregion
